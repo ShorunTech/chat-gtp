1d56706f40e49601d4846beb0164dc4576897cd7
Shoronke Gbenga <gshoronke@gmail.com>
2023-02-03 14:39:03 +0100

    App.js

--- client/src/App.js
+++ client/src/App.js
@@ -32,7 +32,7 @@ function App() {
                        
                   </div>
               <div className="message">
-                Hello World
+                I am AI
               </div>
             </div>
             </div>
