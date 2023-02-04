65a963400c78d9fa5b11a7bd0679fbb0cad81f79
Shoronke Gbenga <gshoronke@gmail.com>
2023-02-04 11:40:59 +0100

    App.css

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <aside className="sidemenu">
-          <div className="side-menu-button">
-          <span>+</span>
-                New Chat
-          </div>
-        </aside>
+      
         <section className="chatbox">
         <div className="chat-log">
             <div className="chat-message chatgpt">
