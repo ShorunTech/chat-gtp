8e07899bb2f38416708d3e8f08ac090e1433ca7a
Shoronke Gbenga <gshoronke@gmail.com>
2023-02-10 13:56:30 +0100

    index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+    organization: "org-lSELnaPPfpTkUXtbpVuRovk6",
+    apiKey: "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",
+});
