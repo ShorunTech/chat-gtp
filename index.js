461fb1bdef018b3dd1ef286a4285f752898d78f5
Shoronke Gbenga <gshoronke@gmail.com>
2023-02-11 20:11:43 +0100

    index.js

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ const configuration = new Configuration({
     organization: "org-lSELnaPPfpTkUXtbpVuRovk6",
     apiKey: "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",
 });
+
+const openai = new OpenAIApi(configuration);
