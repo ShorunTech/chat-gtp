404e67b07a36977527a4c69d274b564586cc9a79
Shoronke Gbenga <gshoronke@gmail.com>
2023-02-13 14:41:51 +0100

    index.js

--- index.js
+++ index.js
@@ -12,6 +12,14 @@ const openai = new OpenAIApi(configuration);
 const app = express()
 const port = 3000 
 
+app.post('/', async (req, res) => {
+  const response = await openai.createComppletion({
+    model: "text-davinci-003",
+    prompt: "Say this is a test",
+    max_tokens: 7,
+    temperature: 0,
+  });
+
 const openai = new OpenAIApi(configuration);
 
 async function callApi() { 
