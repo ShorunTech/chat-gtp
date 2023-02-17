6eef468090d04a2192928374fb80a5ecd1dbaf85
Shoronke Gbenga <gshoronke@gmail.com>
2023-02-17 16:50:38 +0100

    index.js

--- index.js
+++ index.js
@@ -1,49 +1,45 @@
-const { Configuration, OpenAIApi }= require ("openai");
+const { Configuration, OpenAIApi } = require("openai");
 const express = require('express')
 
 const configuration = new Configuration({
     organization: "org-lSELnaPPfpTkUXtbpVuRovk6",
-    apiKey: "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"
-    ,
+    apiKey: "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",
 });
 
 const openai = new OpenAIApi(configuration);
 
 const app = express()
-const port = 3000 
+const port = 3000
 
-app.post('/', async (req, res) => {
-  const response = await openai.createComppletion({
-    model: "text-davinci-003",
-    prompt: "Say this is a test",
-    max_tokens: 7,
-    temperature: 0,
-  });
+app.post('/', async(req, res) => {
+    const response = await openai.createComppletion({
+        model: "text-davinci-003",
+        prompt: "Say this is a test",
+        max_tokens: 7,
+        temperature: 0,
+    });
 
-    console.log(response.data.choices[0].text)
-  res.json({
-    data: response.data
-  })
+
 
 
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 
 });
 
 const openai = new OpenAIApi(configuration);
 
-async function callApi() { 
+async function callApi() {
     const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: "Say this is a test",
-       max_tokens: 7,
-       temperature: 0,
-  });
+        model: "text-davinci-003",
+        prompt: "Say this is a test",
+        max_tokens: 7,
+        temperature: 0,
+    });
 
-  console.log(response.data.choices[0].text)
+    console.log(response.data.choices[0].text)
 
 }
 
